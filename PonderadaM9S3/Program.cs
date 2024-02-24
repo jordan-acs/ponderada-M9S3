1eb1eab2c85671474b08655b67c01781e372a4c4
Jordan Andrade <jordan.silva@sou.inteli.edu.br>
2024-02-23 22:01:48 -0300

    first commit

---
+++ PonderadaM9S3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PonderadaM9S3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
